14f456f1a2a7751d7faba21f76be485cce14f0de
Aritra Banerjee <aritra.banerjee.school@gmail.com>
2025-06-19 02:19:45 +0530

    Update Layer.hpp

--- Digit Recogniser Using Neural Network From Scratch C++/Layer.hpp
+++ Digit Recogniser Using Neural Network From Scratch C++/Layer.hpp
@@ -14,12 +14,16 @@ class Layer
      int input_size;
      int output_size;
 
+     
              
      //store the input from the previous layer
      std::vector<std::vector<double>> inputs;
 
      //store the weights of links of the current layer to next layer
      std::vector<std::vector<double>> weights;
+
+     //biases
+     std::vector<std::vector<double>> biases;
 
      //store the output of this layer which will serve as input 
      //for next layer
@@ -33,17 +37,21 @@ class Layer
              Layer();
              Layer(int input_size, int output_size);
 
-             std::vector<std::vector<double>> forward(const std::vector<std::vector<double>> &input);
+             std::vector<std::vector<double>> forward(const std::vector<std::vector<double>> &input,bool is_ouput_layer);
  
              void compute_error(const std::vector<std::vector<double>>& target);
 
              void update_weights(double learning_rate,const std::vector<std::vector<double>> &error);
+
+             void update_biases(double learning_rate,const std::vector<std::vector<double>> &error);
 
              void compute_error_hidden_layers(const std::vector<std::vector<double>> &next_weights, std::vector<std::vector<double>> &next_delta);
 
              std::vector<std::vector<double>> get_weights();
 
              std::vector<std::vector<double>> get_delta();
+
+             std::vector<std::vector<double>> get_biases();
 
              void save(std::ofstream& out) const;
              
